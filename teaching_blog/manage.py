f23082c20539130c18cc4d5c14e59f83c99f0471
2440042872shravan <71487234+2440042872shravan@users.noreply.github.com>
2024-02-22 12:03:53 +0700

    commit

--- teaching_blog/manage.py
+++ teaching_blog/manage.py
@@ -3,30 +3,20 @@
 import os
 import sys
 
+
 def main():
     """Run administrative tasks."""
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'teaching_blog.settings')
     try:
-        # Check if DJANGO_SETTINGS_MODULE is already set
-        if not os.environ.get('DJANGO_SETTINGS_MODULE'):
-            # If not set, set it to 'teaching_blog.settings'
-            os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'teaching_blog.settings')
-        
-        # Import Django only after setting DJANGO_SETTINGS_MODULE
         from django.core.management import execute_from_command_line
     except ImportError as exc:
-        # Handle ImportError by providing more detailed error message
         raise ImportError(
-            f"Couldn't import Django. {exc}. Are you sure it's installed and "
-            f"available on your PYTHONPATH environment variable? Did you "
-            f"forget to activate a virtual environment?"
+            "Couldn't import Django. Are you sure it's installed and "
+            "available on your PYTHONPATH environment variable? Did you "
+            "forget to activate a virtual environment?"
         ) from exc
-    except Exception as e:
-        # Handle other exceptions gracefully and print out the error
-        print(f"An error occurred: {e}")
-        sys.exit(1)
-
-    # Execute Django management commands
     execute_from_command_line(sys.argv)
+
 
 if __name__ == '__main__':
     main()
--- teaching_blog/teaching_blog/urls.py
+++ teaching_blog/teaching_blog/urls.py
@@ -1,7 +1,8 @@
-"""teaching_blog URL Configuration
+"""
+URL configuration for teaching_blog project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.1/topics/http/urls/
+    https://docs.djangoproject.com/en/5.0/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -15,18 +16,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from django.conf.urls import include
-import app_users
-import curriculum
 
 urlpatterns = [
-    path('', include('app_users.urls')),
-    path('curriculum/',include('curriculum.urls')),
     path('admin/', admin.site.urls),
 ]
-
-from django.conf import settings
-from django.conf.urls.static import static
-if settings.DEBUG:
-        urlpatterns += static(settings.MEDIA_URL,
-                              document_root=settings.MEDIA_ROOT)
